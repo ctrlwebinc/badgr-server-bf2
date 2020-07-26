79abaab7dde0fde5c6956f8dc8864505320bac4f
Daniel Le Blanc <daniel.leblanc@ctrlweb.ca>
2020-07-26 11:58:16 -0400

    initial commit

--- bf2/v2_api_urls.py
+++ bf2/v2_api_urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
 
     url(r'^issuers_count$', views.issuers_count, name='v2_api_issuers_count'),
     url(r'^badgeclasses_count$', views.badgeclasses_count, name='v2_api_badgeclasses_count'),
-
+    url(r'^badgeinstances_count/issuer/(?P<issuer_slug>[^/]+)$', views.badgeinstances_for_issuer_count, name='v2_api_badgeinstances_for_issuer_count'),
+    url(r'^badgeinstances_count/badgeclass/(?P<badgeclass_slug>[^/]+)$', views.badgeinstances_for_badgeclass_count, name='v2_api_badgeinstances_for_badgeclass_count'),
 ]
--- bf2/views.py
+++ bf2/views.py
@@ -1,5 +1,5 @@
 from django.http import JsonResponse
-from issuer.models import (Issuer, BadgeClass)
+from issuer.models import (Issuer, BadgeClass, BadgeInstance)
 
 def issuers_count(request):
     data = {
@@ -10,5 +10,18 @@ def issuers_count(request):
 def badgeclasses_count(request):
     data = {
         'count': BadgeClass.objects.count()
+    }
+    return JsonResponse(data)
+
+def badgeinstances_for_issuer_count(request, issuer_slug):
+    data = {
+        'count': BadgeInstance.objects.filter().count()
+    }
+    return JsonResponse(data)
+
+def badgeinstances_for_badgeclass_count(request, badgeclass_slug):
+    badgeclass = BadgeClass.objects.get(entity_id=badgeclass_slug)
+    data = {
+        'count': BadgeInstance.objects.filter(badgeclass_id=badgeclass.id).count()
     }
     return JsonResponse(data)
