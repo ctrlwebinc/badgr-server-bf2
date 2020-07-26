22221bd5b736661ecdae1657b845144a53fb8b77
Daniel Le Blanc <daniel.leblanc@ctrlweb.ca>
2020-07-26 12:09:56 -0400

    initial commit

--- bf2/views.py
+++ bf2/views.py
@@ -13,15 +13,24 @@ def badgeclasses_count(request):
     }
     return JsonResponse(data)
 
-def badgeinstances_for_issuer_count(request, issuer_slug):
-    data = {
-        'count': BadgeInstance.objects.filter().count()
-    }
+def badgeinstances_for_issuer_count(request, entity_id):
+    try:
+        issuer = Issuer.objects.get(entity_id=entity_id)
+    except Issuer.DoesNotExist:
+        issuer = None
+    if issuer is None:
+        data = {
+        'count': 0
+        }
+    else:
+        data = {
+            'count': BadgeInstance.objects.filter(issuer_id=issuer.id).count()
+        }
     return JsonResponse(data)
 
-def badgeinstances_for_badgeclass_count(request, badgeclass_slug):
+def badgeinstances_for_badgeclass_count(request, entity_id):
     try:
-        badgeclass = BadgeClass.objects.get(entity_id=badgeclass_slug)
+        badgeclass = BadgeClass.objects.get(entity_id=entity_id)
     except BadgeClass.DoesNotExist:
         badgeclass = None
     if badgeclass is None:
